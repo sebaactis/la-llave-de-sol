c74bf6fa5a126cee5909301f64c921531097e482
sebaactis <sebaactis@gmail.com>
2025-02-14 09:50:15 -0300

    fix mobile video

--- components/CallAction.tsx
+++ components/CallAction.tsx
@@ -6,36 +6,35 @@ const CallAction = () => {
   const message = "Hola quiero reservar mi lugar en la llave de sol!"
 
   return (
-    <section className="py-16 my-10" id="call-to-action">
-      <div className='py-20 w-[80%] m-auto flex flex-col justify-center items-center gap-5'>
+    <section className="py-6 md:py-16 my-10" id="call-to-action">
+      <div className='py-5 md:py-20 w-[80%] m-auto flex flex-col justify-center items-center gap-4 md:gap-5'>
         <h2 className='text-2xl font-bold text-black/60 text-center md:text-start'>¡VOS TAMBIÉN PODÉS SER PARTE DEL CAMBIO!</h2>
         <div className='border-b-1 border-black/50 w-[15rem] md:w-[30rem]' />
       </div>
 
-      <div className='w-[100%] m-auto'>
-        <video autoPlay loop muted className="w-full h-[50rem] rounded-md">
-          <source src="https://storage.googleapis.com/msgsndr/XbIroeCo43YzroXkLcgO/media/67289aa684cd8364415c81f7.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
+      <div>
+        <div className='w-[100%] m-auto'>
+          <video controls loop muted className=" w-[90%] md:w-[23%] h-[50rem] rounded-md m-auto">
+            <source src="https://storage.googleapis.com/msgsndr/XbIroeCo43YzroXkLcgO/media/67289aa684cd8364415c81f7.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
+
+        <div className='border-b-1 border-black/50 w-[15rem] md:w-[30rem] m-auto mt-16' />
+
+        <div className='flex flex-col items-center justify-center mt-10 gap-5 bg-[#c4ac92] rounded-md w-[90%] md:w-[50%] py-10 m-auto'>
+          <p className='text-xl text-white text-center md:text-start'>
+            <span className='font-bold mr-1'>Proxima fecha de la experencia:</span>
+            DD/MM/YYYY
+          </p>
+          <p className='text-xl text-white'><span className='font-bold'>Horario:</span> XX.XX Hs</p>
+          <p className='text-xl text-white'><span className='font-bold'>Lugar:</span> Calle 123 - Buenos Aires</p>
+          <a className="flex items-center gap-2 bg-green-500 mt-1 px-3 py-2 rounded-md text-white font-bold transition-all duration-300 hover:bg-green-500/50" href={`https://wa.me/${phoneNumber}?text=${message}`} rel="noopener noreferrer" target="_blank">
+            <FaWhatsapp size={28} />
+            Reservá tu lugar
+          </a>
+        </div>
       </div>
-
-      <div className='border-b-1 border-black/50 w-[15rem] md:w-[30rem] m-auto mt-16' />
-
-      <div className='flex flex-col items-center justify-center mt-10 gap-5 bg-[#c4ac92] rounded-md w-[90%] md:w-[50%] py-10 m-auto'>
-        <p className='text-xl text-white text-center md:text-start'>
-          <span className='font-bold mr-1'>Proxima fecha de la experencia:</span>
-          DD/MM/YYYY
-        </p>
-        <p className='text-xl text-white'><span className='font-bold'>Horario:</span> XX.XX Hs</p>
-        <p className='text-xl text-white'><span className='font-bold'>Lugar:</span> Calle 123 - Buenos Aires</p>
-        <a className="flex items-center gap-2 bg-green-500 mt-1 px-3 py-2 rounded-md text-white font-bold transition-all duration-300 hover:bg-green-500/50" href={`https://wa.me/${phoneNumber}?text=${message}`} rel="noopener noreferrer" target="_blank">
-          <FaWhatsapp size={28} />
-          Reservá tu lugar
-        </a>
-      </div>
-
-
-
     </section>
   )
 }
--- components/Testimonial.tsx
+++ components/Testimonial.tsx
@@ -4,12 +4,12 @@ import img from "@/public/benefitsImg.jpg"
 
 const Testimonial = ({ reverse }: { reverse: Boolean }) => {
     return (
-        <div className={`flex ${reverse ? "md:flex-row-reverse" : "md:flex-row"} flex-col justify-center items-center md:items-start gap-14 md:gap-10`}>
+        <div className={`flex ${reverse ? "md:flex-row-reverse" : "md:flex-row"} flex-col justify-center items-center md:items-start gap-10`}>
             <Image alt="imagen testimonio" className='rounded-md w-[23rem] md:w-[40rem] 2xl:w-[50rem]' src={img} />
             <div className='flex flex-col gap-5 md:w-[40%] items-center md:items-start'>
                 <h4 className='text-2xl font-semibold'>Titulo del testimonio</h4>
-                <p className='md:text-md 2xl:text-xl font-light text-center md:text-start mx-3 md:mx-0'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate totam neque nulla architecto ex officiis, eum error recusandae nostrum velit deleniti, molestias quam corporis placeat. Eaque dignissimos reiciendis iste quam.
-                    Ut porro harum saepe. Repudiandae repellendus sapiente nesciunt ullam esse quia rem non voluptates pariatur doloribus, ab libero at hic facere mollitia fuga, culpa quae quidem, iste laudantium dolores ipsa!</p>
+                <p className='md:text-md 2xl:text-xl font-light text-center md:text-start mx-3 md:mx-0 w-[90%]'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate totam neque nulla architecto ex officiis, eum error recusandae nostrum velit deleniti, molestias quam corporis placeat. Eaque dignissimos reiciendis iste quam.
+                    Ut porro harum saepe. Repudiandae repellendus sapiente nesciunt ullam esse quia rem non voluptates pariatur doloribus, ab libero at hic facere mollitia fuga, culpa quae quidem, iste laudantium dolores ipsa! culpa quae quidem, iste laudantium dolores ipsa! culpa quae quidem, iste laudantium dolores ipsa! culpa quae quidem, iste laudantium dolores ipsa! culpa quae quidem, iste laudantium dolores ipsa! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae, ullam. Asperiores, illo, praesentium quia, dolores debitis ratione corporis cum nihil velit mollitia inventore impedit ipsa facilis eligendi omnis! Distinctio, et! </p>
             </div>
         </div>
     )
