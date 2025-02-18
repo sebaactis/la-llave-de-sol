e131f4270112ee8a427c777740b2fac6127f7920
sebaactis <sebaactis@gmail.com>
2025-02-18 10:42:42 -0300

    change benefits title + change size testimonials description text

--- components/Benefits.tsx
+++ components/Benefits.tsx
@@ -5,7 +5,6 @@ import BenefitCard from './BenefitCard'
 
 import img from "@/public/benefitsImg.jpg"
 
-
 const Benefits = () => {
   return (
     <section className="container mx-auto px-6 py-24 mt-20" id="benefits">
@@ -33,22 +32,20 @@ const Benefits = () => {
         <div className="space-y-4">
           <p className='text-xl ml-1 font-semibold text-gray-700 italic text-center mb-5'>Unite a la experiencia de las 7 llaves para transformar tu relación con la comida y con vos mismo</p>
           <BenefitCard
-            description="Aprenderas a cocinar sin azúcar y sin glúten. Fácil y rápido.
-                    Recetas prácticas y simples que podrás hacer en 5 minutos para resolver tu día de manera espectacular."
+            description="Recetas prácticas y simples que podrás hacer en 5 minutos para resolver tu día de manera espectacular."
             icon={<HeartPulse className="w-8 h-8 text-white" />}
-            title="Medicina en tu cocina"
+            title="Aprenderas a cocinar sin azúcar y sin glúten. Fácil y rápido."
           />
           <BenefitCard
-            description="Descubrirás como el azúcar impacta en tu cuerpo y en tu mente.
+            description="
                     Te enseño que le pasa a tu cerebro, tu intestino y tu energía cuando consumís azúcar y glúten."
             icon={<BrainIcon className="w-8 h-8 text-white" />}
-            title="Somos los que comemos"
+            title="Descubrirás como el azúcar impacta en tu cuerpo y en tu mente."
           />
           <BenefitCard
-            description="Conectarás con lo nos nutre DENTRO y FUERA de tu plato.
-                    A través de ejercicios prácticos, desbloquea hábitos y emociones que hoy te alejan del bienestar. "
+            description="A través de ejercicios prácticos, desbloquea hábitos y emociones que hoy te alejan del bienestar. "
             icon={<Handshake className="w-8 h-8 text-white" />}
-            title="Titulo 3"
+            title="Conectarás con lo nos nutre DENTRO y FUERA de tu plato."
           />
         </div>
       </div>
--- components/Testimonial.tsx
+++ components/Testimonial.tsx
@@ -8,7 +8,7 @@ const Testimonial = ({ reverse }: { reverse: Boolean }) => {
             <Image alt="imagen testimonio" className='rounded-md w-[23rem] md:w-[40rem] 2xl:w-[50rem]' src={img} />
             <div className='flex flex-col gap-5 md:w-[40%] items-center md:items-start'>
                 <h4 className='text-2xl font-semibold'>Titulo del testimonio</h4>
-                <p className='md:text-md 2xl:text-xl font-light text-center md:text-start mx-3 md:mx-0 w-[90%]'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate totam neque nulla architecto ex officiis, eum error recusandae nostrum velit deleniti, molestias quam corporis placeat. Eaque dignissimos reiciendis iste quam.
+                <p className='md:text-md 2xl:text-lg font-light text-center md:text-start mx-3 md:mx-0 w-[90%]'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate totam neque nulla architecto ex officiis, eum error recusandae nostrum velit deleniti, molestias quam corporis placeat. Eaque dignissimos reiciendis iste quam.
                     Ut porro harum saepe. Repudiandae repellendus sapiente nesciunt ullam esse quia rem non voluptates pariatur doloribus, ab libero at hic facere mollitia fuga, culpa quae quidem, iste laudantium dolores ipsa! culpa quae quidem, iste laudantium dolores ipsa! culpa quae quidem, iste laudantium dolores ipsa! culpa quae quidem, iste laudantium dolores ipsa! culpa quae quidem, iste laudantium dolores ipsa! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae, ullam. Asperiores, illo, praesentium quia, dolores debitis ratione corporis cum nihil velit mollitia inventore impedit ipsa facilis eligendi omnis! Distinctio, et! </p>
             </div>
         </div>
