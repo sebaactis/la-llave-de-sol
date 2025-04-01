4bcab65c800045ba57c43069bf5b70b4e10bbae7
sebaactis <sebaactis@gmail.com>
2025-04-01 16:32:01 -0300

    fix video v2

--- components/Presentation.tsx
+++ components/Presentation.tsx
@@ -3,16 +3,16 @@ import { playfairDisplay } from "@/utils/typographies"
 
 const Presentation = () => {
     return (
-        <div className='pt-14 pb-10 w-[100%] md:w-[80%] m-auto -mt-20 flex flex-col justify-center items-center gap-10 bg-[#FF972A] md:rounded-3xl relative mb-14'>
+        <div className='pt-14 pb-10 w-[100%] md:w-[80%] m-auto -mt-20 flex flex-col justify-center gap-10 bg-[#FF972A] md:rounded-3xl relative mb-14'>
             <h2 className={` ${playfairDisplay.className} text-2xl md:text-4xl font-bold text-white text-center mx-6 md:mx-10 -mt-5 md:-mt-0`}>¿POR QUÉ TE CUESTA TANTO DEJAR EL AZUCAR Y EL GLUTEN?  </h2>
 
-            <div className='border-b-1 border-white w-[15rem] md:w-[30rem]' />
+            <div className='border-b-1 border-white w-[15rem] md:w-[30rem] self-center' />
             <div className="mx-2 flex justify-center items-center">
                 <iframe
                     allowFullScreen
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    className="w-full m-auto rounded-xl overflow-hidden aspect-[16/9] md:min-h-[37rem] min-h-[18.2rem]"
-                    src="https://www.youtube.com/embed/EM1S6O225DM?rel=0&modestbranding=1&controls=0&showinfo=0"
+                    className="w-full m-auto rounded-xl overflow-hidden md:min-h-[37rem] min-h-[18.2rem] "
+                    src="https://www.youtube.com/embed/x4B9nPGK78k?rel=0&modestbranding=1&controls=0&showinfo=0"
                     title="YouTube video player" />
             </div>
         </div>
